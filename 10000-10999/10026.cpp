46196653885848ca5f16926d654938743c65e526
seungchulee <inthetelevision@gmail.com>
2019-08-30 20:07:26 +0900

    8/30

--- 10000-10999/10026.cpp
+++ 10000-10999/10026.cpp
@@ -1,67 +1,119 @@
 #include <stdio.h>
-char graph[101][101];
-int visit[101][101];
-
-int dx[4] = {1,-1,0,0};
-int dy[4] = {0,0,1,-1};
+#include <cstring>
 int N;
-void DFS(int i, int j)
+char arr[101][101];
+int visit[101][101];
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,-1,1};
+int cnt_R=0;
+int cnt_G=0;
+int cnt_B=0;
+int cnt_RG=0;
+int cnt_BB=0;
+
+void DFS(int x, int y,char ch)
 {
-	visit[i][j]=1;
-	int ddx,ddy;
-	for(int x=0;x<4;x++)
+	visit[x][y]=1;
+	for(int i=0;i<4;i++)
 	{
-		ddx=i+dx[x];
-		ddy=j+dy[x];
+		int ddx=x+dx[i];
+		int ddy=y+dy[i];
+
 		if(ddx<0 || ddy<0 || ddx>=N || ddy>=N)
-		{
 			continue;
+		if(arr[ddx][ddy]==ch && visit[ddx][ddy]==0)
+		{
+			DFS(ddx,ddy,ch);
 		}
-		if(visit[ddx][ddy]==0 && graph[ddx][ddy]==1)
-			DFS(ddx,ddy);
+
+
 	}
 }
 int main(void)
 {
-	char ch;
+	memset(visit,0,sizeof(visit));
 	scanf("%d",&N);
-
 	for(int i=0;i<N;i++)
 	{
+		char st[101];
+		scanf("%s",st);
 		for(int j=0;j<N;j++)
 		{
-			visit[i][j]=0;
-
-			scanf("%c",&graph[i][j]);
+			arr[i][j]=st[j];
 		}
 	}
-	printf("\n\n");
+	
 	for(int i=0;i<N;i++)
 	{
 		for(int j=0;j<N;j++)
 		{
-			printf("%c",graph[i][j]);
-		}
-		printf("\n");
-	}
-
-	int yes=0;
-	int no=0;
-	for(int i=0;i<N;i++)
-	{
-		for(int j=0;j<N;j++)
-		{
-			if(visit[i][j]==0 && graph[i][j]==1)
+			if(arr[i][j]=='R' && visit[i][j]==0)
 			{
-				DFS(i,j);
-				yes++;
+				cnt_R++;
+				DFS(i,j,'R');
 			}
-//			if(non_visit[i][j]==0 && graph[i][j]>=1)
-//			{
-//				non_DFS(i,j);
-//			}
 		}
 	}
-	printf("%d\n",yes+1);
 
-}
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			if(arr[i][j]=='B' && visit[i][j]==0)
+			{
+				cnt_B++;
+				DFS(i,j,'B');
+			}
+		}
+	}
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			if(arr[i][j]=='G' && visit[i][j]==0)
+			{
+				cnt_G++;
+				DFS(i,j,'G');
+			}
+		}
+	}
+
+
+	memset(visit,0,sizeof(visit));
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			if(arr[i][j]=='G')
+				arr[i][j]='R';
+
+		}
+
+	}
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			if(arr[i][j]=='R' && visit[i][j]==0)
+			{
+				cnt_RG++;
+				DFS(i,j,'R');
+			}
+		}
+	}
+	for(int i=0;i<N;i++)
+	{
+		for(int j=0;j<N;j++)
+		{
+			if(arr[i][j]=='B' && visit[i][j]==0)
+			{
+				cnt_BB++;
+				DFS(i,j,'B');
+			}
+		}
+	}
+	printf("%d %d",cnt_R+cnt_G+cnt_B,cnt_RG+cnt_BB);
+	
+}	
+
+
